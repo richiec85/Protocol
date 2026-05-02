628c83c14e79c46f09d20fe4aac3cbd4e06aa1c0
richiec85 <carragherr@gmail.com>
2026-05-02 18:34:50 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Protocol/',  // Change this to your repo name
+  base: '/Protocol/', // Correct for GitHub Pages
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
   },
 });
